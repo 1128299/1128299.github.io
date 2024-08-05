6d56e9e1acda770a3019b667aced4b76887dafe3
1128299 <1128299@qq.com>
2024-08-05 19:17:48 +0800

    Site updated: 2024-08-05 19:17:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/05/3/","2024/08/03/hello-world/","2024/08/05/聊斋故事：偷鸡不成蚀把米/"];function toRandomPost(){
+var posts=["article/6dd28e9b.html","article/4a17b156.html","article/a081e542.html","article/d48cd2f.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
