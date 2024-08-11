9313f81d8845ff2dc13381511bac72c96cf2d74e
1128299 <1128299@qq.com>
2024-08-11 15:13:01 +0800

    Site updated: 2024-08-11 15:13:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["article/f5d798b8.html","article/3758dd9c.html","article/bd15d44c.html","article/a081e542.html","article/420e678e.html"];function toRandomPost(){
+var posts=["article/3758dd9c.html","article/f5d798b8.html","article/bd15d44c.html","article/a081e542.html","article/420e678e.html","article/d80cc5ed.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
